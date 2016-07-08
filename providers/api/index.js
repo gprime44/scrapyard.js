a9f747fc4531cdd0c58a0b1996310a5fdf5c375b
gprime44 <guillaume.prime@gmail.com>
2016-07-08 11:49:00 +0200

    f

---
+++ providers/api/index.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const request = require('request')
+const cheerio = require('cheerio')
+
+const Settings = require('./settings.js')
+const Extractor = require('./extractor.js')
+const Configurable = require('./configurable.js')
+
+class CPBAPI {
+
+  _createItemObject (item) {
+    return {
+      title: Extractor.getTitle(item),
+      cover: Extractor.getCover(item),
+      seeds: Extractor.getSeeds(item),
+      leechs: Extractor.getLeechs(item),
+      size: Extractor.getSize(item),
+      torrent: Extractor.getTorrentURL(item)
+    }
+  }
+
+  _createPagination (pagination) {
+    return {
+      next: pagination.find('a:last-child').attr('href')
+    }
+  }
+
+  _crawl (URI) {
+    return new Promise((resolve, reject) => {
+      request(URI, (err, res, html) => {
+        if (err) reject(err)
+
+        const $ = cheerio.load(html)
+        const $items = $('#gauche').children('.ligne1, .ligne0')
+
+        const items = []
+        $items.each((sum, item) => {
+          items.push(this._createItemObject($(item)))
+        })
+
+        const pagination = this._createPagination($('#pagination'))
+        resolve({items, pagination})
+      })
+    })
+  }
+
+  Search (query, options) {
+    const params = Configurable.get(Object.assign({scope: 'movies', language: 'FR'}, options))
+    const URL = `${Settings.DOMAIN}/recherche/${params.scope}${params.language}/${encodeURI(query.toLowerCase())}.html`
+    return this._crawl(URL)
+  }
+
+  Top (options) {
+    const params = Configurable.get(Object.assign({scope: 'movies', language: 'FR'}, options))
+    const URL = `${Settings.DOMAIN}/top-100.php?filtre=${params.scope}${params.language}`
+    return this._crawl(URL)
+  }
+
+  Latest (options) {
+    const params = Configurable.get(Object.assign({scope: 'movies', language: 'FR'}, options))
+    const URL = `${Settings.DOMAIN}/view_cat.php?categorie=${params.scope}`
+    return this._crawl(URL)
+  }
+
+}
+
+module.exports = CPBAPI
--- providers/cpasbien.js
+++ providers/cpasbien.js
@@ -5,66 +5,140 @@ var parseTorrent = require('parse-torrent');
 
 var network = require('../network');
 
-const CPBAPI = require('cpasbien-api')
-const api = new CPBAPI()
+const api = require('api/index.js')
 
-exports.movie = function(movieInfo, callback) {
-	api.Search(movieInfo.title).then((values) => {
-		var magnets = [];
-		var nb = 0;
-		for (var i = 0; i < values.items.length; i++) {
+//----------------------------------------------------------------------------
 
-			var magnetInfo = {
-				title:  values.items[i].title,
-				source: 'Cpasbien',
-				seeds:  values.items[i].seeds,
-				peers:  values.items[i].leechs
-			};
-
-			var size = values.items[i].size;
-			var split = size.split(" ");
-			var value = split[0].split(".");
-			if (split[1].startsWith("Ko")) {
-				magnetInfo.size = value[0] * 1024 + value[1];
-			} else if (split[1].startsWith("Mo")) {
-				magnetInfo.size = value[0] * 1024 * 1024 + value[1] * 1024;
-			} else if (split[1].startsWith("Go")) {
-				magnetInfo.size = value[0] * 1024 * 1024 *1024 + value[1] * 1024 * 1024;
-			}
-
-			parseTorrent.remote(values.items[i].torrent, function (err, parsedTorrent) {
-				nb = nb + 1;
-				if (err)  {
-					console.log(err);
-				} else {
-					console.log("Torrent OK : " + parsedTorrent.infoHash);
-					magnetInfo.link = magnet.encode({
-						dn: magnetInfo.title,
-						xt: [ 'urn:btih:' + parsedTorrent.infoHash ],
-						tr: [
-							'udp://tracker.internetwarriors.net:1337',
-							'udp://tracker.coppersurfer.tk:6969',
-							'udp://open.demonii.com:1337',
-							'udp://tracker.leechers-paradise.org:6969',
-							'udp://tracker.openbittorrent.com:80'
-						]
-					});
-	
-					magnets.push(magnetInfo);
-				}
-				
-				if (nb == values.items.length) {
-					console.log("Send callback");
-					callback(null, magnets);
-				}		
-			});
+function parse(item, callback) {
+	parseTorrent.remote(item.torrent, function (err, parsedTorrent) {
+		if (err) {
+			console.log("Error : ", err);
+			callback(err, null);
 		}
+
+		var magnetInfo = {
+				title:  item.title,
+				source: 'Cpasbien',
+				link:   item.torrent,
+				seeds:  item.seeds,
+				peers:  item.leechs
+		};
+
+		var size = item.size;
+		var split = size.split(" ");
+		var value = split[0].split(".");
+		if (split[1].startsWith("Ko")) {
+			magnetInfo.size = value[0] * 1024 + value[1];
+		} else if (split[1].startsWith("Mo")) {
+			magnetInfo.size = value[0] * 1024 * 1024 + value[1] * 1024;
+		} else if (split[1].startsWith("Go")) {
+			magnetInfo.size = value[0] * 1024 * 1024 *1024 + value[1] * 1024 * 1024;
+		}
+
+		magnetInfo.link = magnet.encode({
+			dn: magnetInfo.title,
+			xt: [ 'urn:btih:' + parsedTorrent.infoHash ],
+			tr: parsedTorrent.announce
+		});
+		callback(null, magnetInfo);
 	});
 }
 
+function search(query, options, callback) {
+	api.Search(query, options).then((values) => {
 
+		console.log('Query : %s', query);
+
+		if (values === undefined || values.items.length == 0) {
+			callback(null, []);
+		}
+		async.map(values.items, parse, 
+				function(err, magnets) {
+			callback(err, magnets);
+		}
+		);
+	});
+}
+
+//----------------------------------------------------------------------------
+
+exports.movie = function(movieInfo, callback) {
+
+	async.parallel(
+			[
+			 function(callback) {
+				 search(movieInfo.title, callback);
+			 }
+			 ],
+			 function(err, results) {
+				if (err) {
+					callback(err, null);
+				} else {
+					movieMagnets = [];
+					for (var i = 0; i < results.length; i++) {
+						if(results[i] != null) {
+							movieMagnets = mergeMagnetLists(movieMagnets, results[i]);
+						}
+					}
+					callback(null, movieMagnets);
+				}
+			}
+	);
+}
+
+//----------------------------------------------------------------------------
 
 exports.episode = function(showInfo, seasonIndex, episodeIndex, callback) {
-	 var magnets = [];
-	 callback(null, magnets);
+	async.parallel(
+			[
+			 function(callback) {
+				 console.log('Search tv show episode on cpasbien');
+				 var season = seasonIndex.toString();
+				 if (seasonIndex < 10) {
+					 season = '0' + season;
+				 }
+				 var episode = episodeIndex.toString();
+				 if (episodeIndex < 10) {
+					 episode = '0' + episode;
+				 }
+				 search(util.format('%s-s%s-e%s', showInfo.title, season, episode), {scope: 'tvshow', language: 'EN'}, callback);
+			 }
+			 ],
+			 function(err, results) {
+				if (err) {
+					callback(err, null);
+				} else {
+					episodeMagnets = [];
+					for (var i = 0; i < results.length; i++) {
+						episodeMagnets = mergeMagnetLists(episodeMagnets, results[i]);
+					}
+					callback(null, episodeMagnets);
+				}
+			}
+	);
+}
+
+//----------------------------------------------------------------------------
+
+function mergeMagnetLists(list1, list2) {
+	var toAdd = [];
+
+	if (list2) {
+		for (var i = 0; i < list2.length; i++) {
+			var alreadyAdded = false;
+
+			for (var j = 0; j < list1.length; j++) {
+				if (list2[i].link == list1[j].link) {
+					alreadyAdded = true;
+					break;
+				}
+			}
+
+			if (!alreadyAdded) {
+				toAdd.push(list2[i]);
+			}
+		}
+	}
+
+	return list1.concat(toAdd);
 }
